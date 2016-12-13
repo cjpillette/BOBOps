fbec7aa96139bec5440c391c71baea32d0937a3c
Carole <cjpillette@hotmail.com>
2016-12-14 08:55:27 +1100

    added npm express and hbs

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+// app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('index.hbs');
+});
+
+app.get('/kpi', (req, res) => {
+  res.render('kpi.hbs');
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
